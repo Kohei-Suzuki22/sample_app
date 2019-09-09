c01d65ddc9f2d1fa3997ef2ef8a0639b874ec9df
kohei <3173010@stu.hus.ac.jp>
2019-09-09 15:37:41 +0900

    Add static_pages_contact

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,26 +2,32 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
+  def setup_title_name
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{setup}"
+    assert_select "title", "Home | #{setup_title_name}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{setup}"
+    assert_select "title", "Help | #{setup_title_name}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{setup}"
+    assert_select "title", "About | #{setup_title_name}"
+  end
+
+  test "sould get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{setup_title_name}"
   end
 
 end
