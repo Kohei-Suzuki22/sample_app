7bdaf238101e136b867b83008ddced65099c806b
kohei <3173010@stu.hus.ac.jp>
2019-09-11 21:45:55 +0900

    change routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,31 +7,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "sould get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", setup_title_name
-  end
-
-  test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", setup_title_name
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{setup_title_name}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{setup_title_name}"
   end
 
   test "sould get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{setup_title_name}"
   end
