37f0299ab3631a220a2399d34d80a7a2f11688c0
kohei <3173010@stu.hus.ac.jp>
2019-09-13 08:16:46 +0900

    add bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+  has_secure_password
   before_save {email.downcase!}
 
   validates :name, presence: true, length: {maximum: 50}
@@ -7,5 +8,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true , length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true, length: {minimum: 6}
 
 end
