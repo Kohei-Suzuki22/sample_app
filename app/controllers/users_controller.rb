5b9ecc5ddaaaea6113298c42727d2bf6b105ce59
kohei <3173010@stu.hus.ac.jp>
2019-09-17 22:44:21 +0900

    add sign_up test on failure 'correct post to url'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
+
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,8 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
     get signup_path
+    assert_select "form[action='/signup']"
+
     assert_no_difference "User.count" do
-      post users_path, params: { user:
+      post signup_path, params: { user:
         { name: "",
           email: "user@invalid",
           password: "foo",
@@ -13,16 +15,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
 
+
     assert_template "users/new"
     assert_select "div#error_explanation"
     assert_select "div.alert.alert-danger"
     assert_select "div.field_with_errors"
-    assert_select "ul" do
-      assert_select "li"
+
+    assert_select "ul.errors_ul" do
+      assert_select "li", "Name can't be blank"
+      assert_select "li", "Email is invalid"
+      assert_select "li", "Password is too short (minimum is 6 characters)"
+      assert_select "li", "Password confirmation doesn't match Password"
     end
 
-    # assert_select 'div#CSS id for error explanation '
-    # assert_select "div.<CSS class for field_with_errors>"
   end
 
 end
