675cccc99be7697015dfbbaa4475fede9a51ccbf
kohei <3173010@stu.hus.ac.jp>
2019-09-23 01:01:12 +0900

    add test 'login with valid information'

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template "sessions/new"
@@ -17,4 +21,29 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    assert_template "sessions/new"
+    assert_select "form[action='/login']"
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: "password"
+      }
+    }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "ul.dropdown-menu"
+    assert_select "li.dropdown" do
+      assert_select "a[href=?].dropdown-toggle", "#"
+    end
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", "#"
+    assert_select "a[href=?]", logout_path
+
+  end
+
 end
