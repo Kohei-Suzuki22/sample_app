cac2c4124c601412f82d36ae654cdac9e6671f96
kohei <3173010@stu.hus.ac.jp>
2019-10-04 00:16:09 +0900

    'remember or forget on case' and 'session_params'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: session_params[:email].downcase)
 
-    if user&.authenticate(params[:session][:password])
+    if user&.authenticate(session_params[:password])
       log_in user
-      remember user
+      session_params[:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
@@ -20,4 +20,11 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email,:password,:remember_me)
+  end
+
 end
