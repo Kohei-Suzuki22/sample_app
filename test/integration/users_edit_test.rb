c2cfb8f80eabf8a7c5e8697021b1b8875f9b287d
kohei <3173010@stu.hus.ac.jp>
2019-10-07 14:53:15 +0900

    add users_update on success

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -26,6 +26,31 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "ul.errors_ul li", count: 4
     assert_select "div.alert.alert-danger", text: "The form contains 4 errors"
 
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_select "form[action='#{edit_user_path(@user)}']"
+    assert_template "users/edit"
+    name = "Foo Bar"
+    email = "foo@bar.com"
+
+    patch edit_user_path(@user),params:{
+      user:{
+        name: name,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      }
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
 
   end
 
