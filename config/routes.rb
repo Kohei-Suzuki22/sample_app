40e20d7320d03820f17e3730187aae5e857ffddc
kohei <3173010@stu.hus.ac.jp>
2019-10-07 14:24:37 +0900

    add patch 'users/:id/edit'

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
 
   resources :users
+  patch "/users/:id/edit", to: "users#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,9 +8,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "unsuccessful edit" do
     get edit_user_path(@user)
+    assert_select "form[action='user_path(@user)']", count: 0
+    assert_select "form[action='#{edit_user_path(@user)}']"
     assert_template "users/edit"
 
-    patch user_path(@user), params:{
+    patch edit_user_path(@user), params:{
       user:{
         name: "",
         email: "foo@invalid",
