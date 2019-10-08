6cb77b9030693a2ac8075a8960adf68f0febb9e9
kohei <3173010@stu.hus.ac.jp>
2019-10-09 02:44:22 +0900

    add redirect_back to request.original_url

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -64,5 +64,33 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   end
 
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_redirected_to login_url
+    follow_redirect!
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+
+    name = "Foo Bar"
+    email = "foo@bar.com"
+
+    patch edit_user_path(@user), params:{
+      user:{
+        name: name,
+        email: email,
+        password: "",
+        password_confirmation: ""
+      }
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+
+
+  end
+
 
 end
