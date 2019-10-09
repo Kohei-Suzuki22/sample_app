edc46debc5d7ebacdc1630b36c41c092d49848e6
kohei <3173010@stu.hus.ac.jp>
2019-10-09 16:35:55 +0900

    add users_index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should get index with logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_template "users/index"
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
