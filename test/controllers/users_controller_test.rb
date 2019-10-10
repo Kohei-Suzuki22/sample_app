cc9ccbeb492d6cb1686cb47dc0b5156deae107f1
kohei <3173010@stu.hus.ac.jp>
2019-10-10 18:07:10 +0900

    add test not_admin_user can't toggle admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -63,5 +63,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch edit_user_path(@other_user), params:{
+      user:{
+        password: "",
+        password_confirmation: "",
+        admin: true
+      }
+    }
+    assert_not @other_user.reload.admin?
+
+  end
+
 
 end
