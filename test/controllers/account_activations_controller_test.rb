d1829887f1c493d0872f07f78d788508c24afc71
kohei <3173010@stu.hus.ac.jp>
2019-10-12 12:42:19 +0900

    rails g controller AccountActivations edit

---
+++ test/controllers/account_activations_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get edit" do
+    user = users(:michael)
+    get edit_account_activation_url(user)
+    assert_response :success
+  end
+
+end
