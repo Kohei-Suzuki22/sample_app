82ec358bb72199dc79916d88dedbb475064e0952
kohei <3173010@stu.hus.ac.jp>
2019-10-20 20:21:04 +0900

    add intagration_test 'users_index' with activated

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @non_activated = users(:bob)
   end
 
   test "index as admin including pagination and delete links" do
@@ -13,12 +14,15 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template "users/index"
     assert_select "ul.pagination", count: 2
 
-    first_page_of_users = User.paginate(page: 1, per_page: 20)
+    first_page_of_users = User.where(activated: true).paginate(page: 1, per_page: 20)
     first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
       assert_select "a[href=?]", user_path(user), text: "delete" unless user == @admin
+      assert user.activated?
     end
     assert_select "a[href=?]", user_path(@admin), text: "delete", count: 0
+    assert_select "a[href=?]", user_path(@non_activated), count: 0
+    assert_select @non_activated.name, count: 0
 
     assert_difference "User.count", -1 do
       delete user_path(@non_admin)
@@ -30,12 +34,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
     log_in_as(@non_admin)
     get users_path
-    first_page_of_users = User.paginate(page: 1, per_page: 20)
+    first_page_of_users = User.where(activated: true).paginate(page: 1, per_page: 20)
     first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
+      assert user_path(@non_admin)
     end
     assert_select 'a', text: "delete", count: 0
-
+    assert_select "a[href=?]", user_path(@non_activated), count: 0
+    assert_select @non_activated.name, count: 0
   end
 
 end
