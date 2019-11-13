0bc86f1908ed73b50d0933c9d9fc83c697b6e9a2
kohei <3173010@stu.hus.ac.jp>
2019-11-14 08:32:43 +0900

    add view 'password_resets/new'

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :password_resets, only: [:new, :create, :edit, :update]
   post "/password_resets/new", to: "users#create"
-  put "/password_resets/new", to: "users#create"
   post "/password_resets/:id/edit", to: "users#update"
   put "/password_resets/:id/edit", to: "users#update"
 end
