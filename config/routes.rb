0bd7488f64c03011e1cb59778caf357d41bddce9
koheisuzuki <3173010@stu.hus.ac.jp>
2019-11-27 19:24:24 +0900

    add follow_form and followings_followers_data to views

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,17 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
 
-  resources :users, except: [:update]
+  resources :users, except: [:update] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+
   patch "/users/:id/edit", to: "users#update"
   put "/users/:id/edit", to: "users#update"
+
+  resources :relationships, only: [:create, :destroy]
 
   resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
