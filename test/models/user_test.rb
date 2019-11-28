b6275e2ac9d64fa4c056d77a30234430f450c89e
koheisuzuki <3173010@stu.hus.ac.jp>
2019-11-28 19:45:12 +0900

    repair feed to include 'following_ids and self_id'

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -97,4 +97,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not archer.followed_by?(michael)
   end
 
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+
+    lana.microposts.each do |post_by_following_user|
+      assert michael.feed.include?(post_by_following_user)
+    end
+
+    michael.microposts.each do |post_by_self|
+      assert michael.feed.include?(post_by_self)
+    end
+
+    archer.microposts.each do |post_by_unfollowing_user|
+      assert_not michael.feed.include?(post_by_unfollowing_user)
+    end
+
+  end
+
 end
